be64c2b37d26fb2793a30925e7303461f97a4eec
Tomer Aberbach <tomeraberbach@gmail.com>
2022-11-06 20:12:41 -0500

    fix: remove duplicative rule

--- src/index.js
+++ src/index.js
@@ -383,7 +383,6 @@ export default [
       'sonarjs/no-empty-collection': `error`,
       'sonarjs/no-extra-arguments': `error`,
       'sonarjs/no-identical-conditions': `error`,
-      'sonarjs/no-identical-expressions': `error`,
       'sonarjs/no-ignored-return': `error`,
       'sonarjs/no-use-of-empty-return-value': `error`,
     },
